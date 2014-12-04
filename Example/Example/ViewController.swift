267675eb1d000a87203a3179b1239b57b65c31bd
Jesse Squires <jesse.d.squires@gmail.com>
2014-12-03 22:18:14 -0800

    update example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -17,11 +17,40 @@
 //
 
 import UIKit
+import JSQNotificationListenerKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    var listener: NotificationListener? = nil
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        listener = NotificationListener(notificationName: "CustomNotification", handler: { (notification) -> Void in
+            // handle notification
+        })
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        listener = nil
     }
 }
 
+//    override func viewWillAppear(animated: Bool) {
+//        super.viewWillAppear(animated)
+//        
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleNotification", name: "CustomNotification", object: nil)
+//    }
+//    
+//    override func viewWillDisappear(animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        
+//        NSNotificationCenter.defaultCenter().removeObserver(self, name: "CustomNotification", object: nil)
+//    }
+//    
+//    private func handleNotification:(notif: NSNotification) {
+//        // handle notification
+//    }
+//}
