2890c9798f6a697cbdd412e53a59598859b28fe8
Jesse Squires <jesse.d.squires@gmail.com>
2015-03-23 00:08:30 -0700

    implement new notification and observer

--- JSQNotificationObserverKit/JSQNotificationObserverKit/NotificationObserver.swift
+++ JSQNotificationObserverKit/JSQNotificationObserverKit/NotificationObserver.swift
@@ -18,7 +18,8 @@
 
 import Foundation
 
-public struct Notification<T> {
+
+public struct Notification <Value, Sender: AnyObject> {
 
     public let name: String
 
@@ -26,4 +27,60 @@ public struct Notification<T> {
         self.name = name
     }
 
+}
+
+
+public func postNotification<V, S: AnyObject> (notification: Notification<V, S>, #value: V, sender: S? = nil, center: NSNotificationCenter = NSNotificationCenter.defaultCenter()) {
+    center.postNotificationName(notification.name, object: sender, userInfo: userInfo(value))
+}
+
+
+public final class NotificationObserver <V, S: AnyObject> {
+
+    public typealias Handler = (value: V, sender: S?) -> Void
+
+    private let observerProxy: NSObjectProtocol
+
+    private let center: NSNotificationCenter
+
+    public init(notification: Notification<V, S>, sender: S? = nil, queue: NSOperationQueue? = nil, center: NSNotificationCenter = NSNotificationCenter.defaultCenter(), handler: Handler) {
+        self.center = center
+
+        observerProxy = center.addObserverForName(notification.name, object: sender, queue: queue, usingBlock: { (notification: NSNotification!) -> Void in
+            if let value: V = unboxUserInfo(notification.userInfo) {
+                handler(value: value, sender: notification.object as? S)
+            }
+        })
+    }
+
+    deinit {
+        center.removeObserver(observerProxy)
+    }
+}
+
+// MARK: Private
+
+// Key for user info dictionary
+private let UserInfoValueKey = "value"
+
+// This class allows the "boxing up" instances that are not objects
+private class Box<T> {
+    let value: T
+
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
+// Create user info dictionary
+private func userInfo<T>(value: T) -> [String : Box<T>] {
+    return [UserInfoValueKey : Box(value)]
+}
+
+// Unbox user info dictionary
+private func unboxUserInfo<T>(userInfo: [NSObject: AnyObject]?) -> T? {
+    if let box = userInfo?[UserInfoValueKey] as? Box<T> {
+        return box.value
+    }
+    return nil
 }
--- JSQNotificationObserverKit/JSQNotificationObserverKitTests/JSQNotificationObserverKitTests.swift
+++ JSQNotificationObserverKit/JSQNotificationObserverKitTests/JSQNotificationObserverKitTests.swift
@@ -20,14 +20,47 @@ import XCTest
 import JSQNotificationObserverKit
 
 
+// MARK: Fakes
+
+class FakeSender {
+    let sender = NSUUID().UUIDString
+}
+
+struct FakeValue {
+    let value = NSUUID().UUIDString
+}
+
+
+// MARK: Test case
+
 class JSQNotificationObserverKitTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
+
+    func test() {
+
+        // GIVEN: a notification, sender, and observer
+        let notification = Notification<FakeValue, FakeSender>(name: "\(__FUNCTION__)")
+
+        let fakeSender = FakeSender()
+
+        let fakeValue = FakeValue()
+
+        let expectation = self.expectationWithDescription("\(__FUNCTION__)")
+
+        let observer = NotificationObserver(notification: notification, sender: fakeSender) { (value, sender) -> Void in
+
+            XCTAssertEqual(value.value, fakeValue.value, "Values should be equal")
+            XCTAssertEqual(fakeSender.sender, sender!.sender, "Senders should be equal")
+
+            expectation.fulfill()
+        }
+
+        // WHEN: we post the notification
+        postNotification(notification, value: fakeValue, sender: fakeSender)
+
+        // THEN: the notification is received
+        self.waitForExpectationsWithTimeout(2, handler: { (error) -> Void in
+            XCTAssertNil(error, "Expectation should not error")
+        })
     }
     
 }
