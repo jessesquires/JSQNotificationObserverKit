0dbdd34fd2bc07d8f2e970ac4cc0aa83e76a63c3
Jesse Squires <jesse.d.squires@gmail.com>
2015-04-23 12:28:52 -0700

    update docs. edit script for latest jazzy.

--- build_docs.sh
+++ build_docs.sh
@@ -5,6 +5,4 @@
 # https://github.com/realm/jazzy
 # ------------------------------
 
-cd JSQNotificationObserverKit
-jazzy -o ../_docs -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQNotificationObserverKit' -g 'https://github.com/jessesquires/JSQNotificationObserverKit'
-cd ..
+jazzy -o ./_docs --source-directory ./JSQNotificationObserverKit --readme ./README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQNotificationObserverKit' -g 'https://github.com/jessesquires/JSQNotificationObserverKit'
