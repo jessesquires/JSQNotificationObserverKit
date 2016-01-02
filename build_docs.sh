992417297c0573819b85506a212314f950f1a5e7
Jesse Squires <jesse.d.squires@gmail.com>
2016-01-02 11:51:02 +0100

    update build script

--- build_docs.sh
+++ build_docs.sh
@@ -6,7 +6,7 @@
 
 git submodule update --remote
 
-jazzy --swift-version 2.1 -o ./ \
+jazzy --swift-version 2.1.1 -o ./ \
       --source-directory JSQNotificationObserverKit/JSQNotificationObserverKit/ \
       --readme JSQNotificationObserverKit/README.md \
       -a 'Jesse Squires' \
