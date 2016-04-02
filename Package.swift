a407fe2904ca91b59dfa237c7c40a82c3f942e4d
Jesse Squires <jessesquires@users.noreply.github.com>
2016-04-02 14:26:46 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,21 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQNotificationObserverKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQNotificationObserverKit
+//
+//
+//  License
+//  Copyright © 2015-present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
 import PackageDescription
 
 let package = Package(
